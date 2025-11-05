a9c2ee072451c4bb1286d3e1a5688b3a34d37b9b
lpr-filippo <lopresti.filippo@outlook.com>
2025-11-05 11:31:02 +0100

    feat(main): add test setup for nearest cities and travel time

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ Modules:
 """
 
 from import_data import *
+from utils import *
 
-# importing raw data from kuggle
+# importing raw data from kaggle
 raw_data: pd.DataFrame = import_data(path)
 
 # data cleaning
@@ -19,3 +20,16 @@ raw_data: pd.DataFrame = import_data(path)
 data: pd.DataFrame = raw_data.dropna(subset=["Population","City"])
 # if two cities have the same name it drop the one with the lowest population
 data = data.loc[data.groupby("City")["Population"].idxmax()].reset_index(drop=True)
+
+# initialization
+start_point: pd.DataFrame = data[data["City"] == "london"]
+
+# calculate neighboring cities to London
+neighbors: pd.DataFrame = calculate_neighbors(start_point, data, delta=1)
+
+# Extract three nearest city
+near3 = get_top3(neighbors)
+
+# calculate travel time
+near3["Time"] = (near3.apply(lambda row:
+                             calculate_time(row, start_point["Country"].iloc[0]), axis = 1)) #FIXME cambiare start_point con current quando si fa il ciclo
