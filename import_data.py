87e47909af35854b6eb878408b53d3a4dfd0d81c
lpr-filippo <lopresti.filippo@outlook.com>
2025-11-15 17:34:26 +0100

    feat(import): move data-cleaning into import_data
    Added creation of the new `City_Country` variable and moved the data-cleaning
    pipeline directly inside `import_data, simplifying `main.py`

--- import_data.py
+++ import_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import kagglehub
 import os
 
+import ids
+
 # defining path for data import
 path = kagglehub.dataset_download("max-mind/world-cities-database")
 
@@ -18,3 +20,14 @@ def import_data(path: str) -> pd.DataFrame:
         pd.DataFrame: A DataFrame containing the world cities data.
     """
     return pd.read_csv(os.path.join(path, 'worldcitiespop.csv'), dtype={'Region': str})
+
+raw_data = import_data(path)
+# data cleaning
+# drop obs with NA value in 'Population' or 'City'
+cities_data: pd.DataFrame = raw_data.dropna(subset=["Population", "City"])
+# since multiples cities have the same name create a new variable 'City_Country'
+cities_data.insert(2,
+                   "City_Country",
+                   cities_data["AccentCity"] + " " + cities_data["Country"].str.upper())
+
+cities_data = cities_data.loc[cities_data.groupby(ids.PLACE)["Population"].idxmax()].reset_index(drop=True)
