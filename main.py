cd9578fdcebd4122d4a22b4262e3e9a21ae684e6
lpr-filippo <lopresti.filippo@outlook.com>
2025-11-15 17:59:39 +0100

    feat(main): add `move_atw` and `main` functions
    Defined `move_atw` to handle the main travel loop and created `main` for easier integration with the web app.

--- main.py
+++ main.py
@@ -7,74 +7,78 @@ Main script for the "Around the World" project.
 Modules:
     import_data: Handles dataset import.
     utils: Provides utility functions.
+    ids: Defines variables for easy modification.
+    app_render: Code for the web app using Dash.
+    map_creator: Functions to generate maps and render them in app_render.
 """
+import pandas as pd
 
-from import_data import *
 from utils import calculate_neighbors, calc_neighbors_home, calculate_time, get_top3, create_move, fastest_long, go_home
+import ids
+# importing the clean dataset with the cities
+from import_data import cities_data
 
-# importing raw data from kaggle
-raw_data: pd.DataFrame = import_data(path)
 
-# data cleaning
-# drop obs with NA value in 'Population' or 'City'
-data: pd.DataFrame = raw_data.dropna(subset=["Population","City"])
-# if two cities have the same name it drop the one with the lowest population
-data = data.loc[data.groupby("City")["Population"].idxmax()].reset_index(drop=True)
+def move_atw(data, str_city) -> pd.DataFrame:
 
-# initialization
-start_point: pd.DataFrame = data[data["City"] == "london"]
-index = 0
-current_point = start_point.copy()
+    # initialization
+    start_point: pd.DataFrame = data[data[ids.PLACE] == str_city]
+    index = 0
+    current_point = start_point.copy()
 
-# creating a dataframe with visited cities
-trip = start_point.copy()
-# Initialize the trip DataFrame with additional numeric columns
-# to ensure consistent structure when concatenating next cities
-trip[["Dist_long", "Distance_km", "Time", "Speed"]] = 0.0
+    # creating a dataframe with visited cities
+    trip = start_point.copy()
+    # Initialize the trip DataFrame with additional numeric columns
+    # to ensure consistent structure when concatenating next cities
+    trip[["Dist_long", "Distance_km", "Time", "Speed"]] = 0.0
 
-old_point = pd.DataFrame()
+    while True:
 
-while True:
+        print(f"Current city: {current_point[ids.PLACE].iloc[0]}")
 
-    print(f"Current city: {current_point['City'].iloc[0]}")
+        if (start_point["Longitude"].iloc[0] - ids.DELTA_HOME <= current_point["Longitude"].iloc[0] <= start_point["Longitude"].iloc[0])\
+                and index != 0:
+            # Use calc_neighbors_home when is near home
+            neighbors: pd.DataFrame = calc_neighbors_home(current_point, data, start_point, trip, delta=1, verbose=True)
+            # Extract three nearest city
+            near3 = get_top3(neighbors)
+            if near3.shape[0] == 0:
+                return trip
+            # calculate travel time
+            near3["Time"] = near3.apply(lambda row: calculate_time(row, current_point["Country"].iloc[0]), axis=1)
+            # Select next city prioritizing movement toward home
+            next_point = create_move(near3, lambda df: go_home(df, str_city))
+        else:
+            # Normal eastward travel
+            neighbors: pd.DataFrame = calculate_neighbors(current_point, data, trip, delta=1, verbose=True)
+            # Extract three nearest city
+            near3 = get_top3(neighbors)
+            # calculate travel time
+            near3["Time"] = near3.apply(lambda row: calculate_time(row, current_point["Country"].iloc[0]), axis=1)
+            # Select next city with fastest_long logic
+            next_point = create_move(near3, fastest_long)
 
-    if start_point["Longitude"].iloc[0] - 10.0 <= current_point["Longitude"].iloc[0] < start_point["Longitude"].iloc[0]:
-        # Use calc_neighbors_home when is near home
-        neighbors: pd.DataFrame = calc_neighbors_home(current_point, data, start_point, old_point, delta=1, verbose=True)
-        # Exclude already visited cities except home
-        neighbors = neighbors[~neighbors["City"].isin(trip["City"].iloc[1:])]
-        # Extract three nearest city
-        near3 = get_top3(neighbors)
-        # calculate travel time
-        near3["Time"] = near3.apply(lambda row: calculate_time(row, current_point["Country"].iloc[0]), axis=1)
-        # Select next city prioritizing movement toward home
-        next_point = create_move(near3, lambda df: go_home(df, "london"))
-    else:
-        # Normal eastward travel
-        neighbors: pd.DataFrame = calculate_neighbors(current_point, data, delta=1, verbose=True)
-        # Exclude already visited cities except home
-        neighbors = neighbors[~neighbors["City"].isin(trip["City"].iloc[1:])]
-        # Extract three nearest city
-        near3 = get_top3(neighbors)
-        # calculate travel time
-        near3["Time"] = near3.apply(lambda row: calculate_time(row, current_point["Country"].iloc[0]), axis=1)
-        # Select next city with fastest_long logic
-        next_point = create_move(near3, fastest_long)
+        # convert pd.Series to single-row pd.DataFrame for concatenation with 'trip'
+        next_point = next_point.to_frame().T
+        # add 'next_point' to trip
+        trip = pd.concat([trip, next_point], ignore_index=True)
 
-    # convert pd.Series to single-row pd.DataFrame for concatenation with 'trip'
-    next_point = next_point.to_frame().T
-    # add 'next_point' to trip
-    trip = pd.concat([trip, next_point], ignore_index=True)
+        current_point = next_point.copy()
+        index += 1
 
-    old_point = current_point.copy()
-    current_point = next_point.copy()
-    index += 1
+        # Stop conditions
+        if current_point[ids.PLACE].iloc[0] == str_city and index > 0:
+            print(f"Returned to {str_city}!")
+            break
 
-    # Stop conditions
-    if current_point["City"].iloc[0].lower() == "london" and index > 0:
-        print("Returned to London!")
-        break
+        if index > 10000:
+            print("Too many iterations, stopping.")
+            break
 
-    if index > 10000:
-        print("Too many iterations, stopping.")
-        break
+    return trip
+
+def main():
+    move_atw(cities_data, 'london')
+
+if __name__ == "__main__":
+    main()
