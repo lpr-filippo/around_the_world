6250f85637dc1558788ac43f1eab3a95b507c77b
lpr-filippo <lopresti.filippo@outlook.com>
2025-11-26 15:29:49 +0100

    feat(import_data): merge full country names
    Import the ISO country codes CSV and merge the country names with the cities data on the Alpha-2 code.

--- import_data.py
+++ import_data.py
@@ -5,7 +5,8 @@ import os
 import ids
 
 # defining path for data import
-path = kagglehub.dataset_download("max-mind/world-cities-database")
+path1 = kagglehub.dataset_download("max-mind/world-cities-database")
+path2 = kagglehub.dataset_download("juanumusic/countries-iso-codes")
 
 def import_data(path: str) -> pd.DataFrame:
     """Imports the world cities dataset from the specified directory.
@@ -21,7 +22,7 @@ def import_data(path: str) -> pd.DataFrame:
     """
     return pd.read_csv(os.path.join(path, 'worldcitiespop.csv'), dtype={'Region': str})
 
-raw_data = import_data(path)
+raw_data = import_data(path1)
 # data cleaning
 # drop obs with NA value in 'Population' or 'City'
 cities_data: pd.DataFrame = raw_data.dropna(subset=["Population", "City"])
@@ -31,3 +32,14 @@ cities_data.insert(2,
                    cities_data["AccentCity"] + " " + cities_data["Country"].str.upper())
 
 cities_data = cities_data.loc[cities_data.groupby(ids.PLACE)["Population"].idxmax()].reset_index(drop=True)
+
+# importing a dataset with full country names with associated Alpha-2 code
+cntry_names = pd.read_csv(os.path.join(path2, 'wikipedia-iso-country-codes.csv'))
+# changing the column name with a shorter one
+cntry_names = cntry_names.rename(columns={"English short name lower case": 'Country name'})
+# creating a temporary column for merging
+cities_data['Country_upper'] = cities_data['Country'].str.upper()
+# merging the datasets
+cities_data = cities_data.merge(cntry_names, left_on='Country_upper', right_on='Alpha-2 code', how='left')
+# deleting unnecessary columns
+cities_data = cities_data.drop(columns=['Country_upper', 'Alpha-2 code', 'Alpha-3 code', 'Numeric code', 'ISO 3166-2'])
